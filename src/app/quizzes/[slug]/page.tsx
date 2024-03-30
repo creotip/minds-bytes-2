270e450803812deb74fc4c30a2b4928befbc4f43
creotip <creotip@gmail.com>
2024-03-30 09:29:40 +0300

    vercel analytics

--- src/app/quizzes/[slug]/page.tsx
+++ src/app/quizzes/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation'
 
 type Props = {
   params: { id: string; slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: Record<string, string | string[] | undefined>
 }
 
 export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
